a544857edf4aef91718eeb53bab6b5ff8a05b892
Lalu Hilmi Fadli <heri170890@gmail.com>
2020-10-15 17:56:20 +0800

    Initial Commit

---
+++ Machine Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine Gallery
+//
+//  Created by laluheri on 10/15/20.
+//  Copyright © 2020 hazard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
